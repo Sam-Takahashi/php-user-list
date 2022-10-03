79bfae7cc746e84e14970c50bf8b8b3e8b8998e5
Sam Takahashi <sam.takahashi.sam@gmail.com>
2022-10-03 19:55:00 +0900

    initial commit

--- dbcon.php
+++ dbcon.php
@@ -1,13 +1,13 @@
 <?php
+require 'vendor/autoload.php';
 
-$server = "sql205.epizy.com";
-$username = "epiz_32708529";
-$password = "Gmppr6MR9ZyLG";
-$dbname = "epiz_32708529_blogz";
-// $server = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "students_blog";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$server = $_ENV['SERVER'];
+$username = $_ENV['USERNAME'];
+$password = $_ENV['PASSWORD'];
+$dbname = $_ENV['DBNAME'];
 
 $con = mysqli_connect($server, $username, $password, $dbname);
 
