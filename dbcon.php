c3f840482ad83aac4fdf159c6a2437128ffcd1c1
Sam Takahashi <sam.takahashi.sam@gmail.com>
2022-10-03 17:24:16 +0900

    db config

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,15 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","students_blog");
+$server = "sql205.epizy.com";
+$username = "epiz_32708529";
+$password = "Gmppr6MR9ZyLG";
+$dbname = "epiz_32708529_blogz";
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "students_blog";
+
+$con = mysqli_connect($server, $username, $password, $dbname);
 
 if(!$con) {
   die("Connection Failed". mysqli_connect_error());
